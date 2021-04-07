62ea8bf9fe205f232c21c5d2cd2b3fdd56cb356a
Santiago <santiagorosales1@outlook.com>
2021-04-07 15:02:00 -0300

    apollo instalado

---
+++ client/src/components/categories/Categories.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+ 
+const Categories = ():JSX.Element => {
+
+     let categorie:string[] = ["Placa De Video", "Memorias RAM", "Micropocesadores"]
+    
+     return (
+        <h1>Hola</h1>
+    )
+}
+
+export default Categories;
