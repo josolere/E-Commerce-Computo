98b738a9ddbd2c1b6a21e038028e66d8f7064570
juancordoba <juancordoba75@gmail.com>
2021-04-08 16:17:41 -0300

    configuracion del server y modelos Product y Category

---
+++ api/src/models/index.ts
@@ -0,0 +1,33 @@
+import {Sequelize} from 'sequelize'
+
+import {CategoryFactory, Category as CategoryClass} from './Category'
+import {ProductFactory, Product as ProductClass} from './Product'
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+export interface DB {
+    sequelize : Sequelize,
+    Product: typeof ProductClass,
+    Category: typeof CategoryClass
+} 
+
+const {DB_NAME, DB_PORT, DB_PASSWORD, DB_URL, DB_USER} = process.env
+
+export const sequelize = new Sequelize(
+    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_URL}:${DB_PORT}/${DB_NAME}`
+)
+
+const Product = ProductFactory(sequelize);
+const Category = CategoryFactory(sequelize);
+
+Product.belongsToMany(Category,{through: 'productsxcategories'});
+Category.belongsToMany(Product,{through: 'productsxcategories'})
+
+const db : DB = {
+    sequelize,
+    Product,
+    Category,
+};
+
+export default db;
