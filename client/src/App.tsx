b04660fcd918a35847cebf31e6ae0c11a7224a0a
joseSoler <jossessoler@gmail.com>
2021-04-10 19:34:06 -0500

    navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import NavBar from './components/NavBar/NavBar';
+import ShoppingCart from './components/ShoppingCart/ShoppingCart';
+import PageNotFound from './PageNotFound';
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div >
-      <h1>Hola</h1>
-    </div>
+
+    <Router>
+      <Switch>
+        <Route exact path='/home' component={Home} />
+        <Route exact path='/carrito' component={ShoppingCart} />
+        <Route component={PageNotFound} />
+      </Switch>
+    </Router>
   );
 }
 
