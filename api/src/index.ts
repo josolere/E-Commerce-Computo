6e27dd152ef6affd3cc4acaca2539319329a7aa5
AndresLLogares <andresl940@hotmail.com>
2021-04-11 23:16:27 -0300

    servidor3

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,14 +1,18 @@
 import { sequelize } from "./models/index";
 import app from "./app";
 import apolloServer from "./apollo";
+import { productsSeeder } from "./seeders/test";
 
 require("dotenv").config();
 const { PORT } = process.env;
 
 const alter = true;
-const force = false;
+const force = true;
 const logging = true;
 sequelize.sync({ alter, force, logging }).then(() => {
+  if (force) {
+    productsSeeder()
+  }
   apolloServer.applyMiddleware({ app });
   app.listen(PORT, () => {
     console.log(`\nRunning Playground on ${PORT}/graphql`);
