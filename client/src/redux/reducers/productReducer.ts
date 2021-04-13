d6fccd49ad5ea5e26fa3d64fce3a42e010fa2115
CrisSoria <crissoria07@gmail.com>
2021-04-13 11:27:47 -0300

    solucionado conflicto de merge

--- client/src/redux/reducers/productReducer.ts
+++ client/src/redux/reducers/productReducer.ts
@@ -1,29 +1,25 @@
-import {SET_FILTER, SET_CATEGORIES} from '../actions'
+import { SET_FILTER, SET_CATEGORIES } from "../actions";
 
 const initialState = {
-    filter:"",
-    categories:[]
-}
+  filter: "",
+  categories: [],
+};
 
-function productReducer(state = initialState, action:any):any{
-
-    switch(action.type){
-        case SET_FILTER:
-            return {
-                ...state,
-                filter: action.filter
-            }
-<<<<<<< HEAD
-            case SET_ACTIVITIES:
-=======
-            case SET_CATEGORIES:
->>>>>>> c905d7cd8d645423639b8429a0fdda2401420957
-                return {
-                    ...state,
-                    categories : action.categories
-                }
-        default: return state;
-    }
+function productReducer(state = initialState, action: any): any {
+  switch (action.type) {
+    case SET_FILTER:
+      return {
+        ...state,
+        filter: action.filter,
+      };
+    case SET_CATEGORIES:
+      return {
+        ...state,
+        categories: action.categories,
+      };
+    default:
+      return state;
+  }
 }
 
 export default productReducer;
