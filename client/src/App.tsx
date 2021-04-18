b86346f6a7fc1faa4cb04621a832a0ef826b8d3f
AndresLLogares <andresl940@hotmail.com>
2021-04-18 02:59:48 -0300

    fin dia de mierda

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,8 +4,6 @@ import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import Details from './components/Details/ProductsDetails'
 import LandPage from './components/landpage/LandPage'
 import Payment from './components/payment/Stripe'
-import CreateProduct from './components/CreateProduct/CreateProduct';
-import CreateCategory from './components/CreateCategory/CreateCategory';
 import Home from './components/Home/Home'
 import PageNotFound from './components/PageNotFound/PageNotFound'
 import ShoppingCart from './components/ShoppingCart/ShoppingCart'
@@ -40,8 +38,6 @@ function App() {
       </Route>
       <Switch>
         <Route exact path='/Crear'component={Unicrear} />
-  {/*       <Route exact path='/CrearCategoria' component={CreateCategory} />
-        <Route exact path='/CrearProducto' component={CreateProduct} /> */}
         <Route exact path='/Detalles' component={Details} />
         <Route exact path='/Login' component={Login} />
         <Route exact path='/Pago' component={Payment} />
