338a08ca19e98f6dc705954ce76cc41a5aab5b86
CrisSoria <crissoria07@gmail.com>
2021-04-20 11:00:15 -0300

    correccion login

--- api/src/graphql/resolvers/user.ts
+++ api/src/graphql/resolvers/user.ts
@@ -1,14 +1,14 @@
 import {
-  iCreateUserInput,
+  // iCreateUserInput,
   iSignUpInput,
   iEditUserInput,
   iModels,
   iUser,
 } from "../../interfaces";
-import Sequelize, { Op } from "sequelize";
-import User from "../../models";
-import db from "../../models";
-import {v4 as uuid} from 'uuid'
+// import Sequelize, { Op } from "sequelize";
+// import User from "../../models";
+// import db from "../../models";
+import { v4 as uuid } from "uuid";
 
 export default {
   Query: {
@@ -20,25 +20,25 @@ export default {
       const data = await models.User.findByPk(id);
       return data;
     },
-    currentUser: (parent:object, args:any, context:any) => context.getUser(),
-    
-    
-    getUsers: async(
+
+    currentUser: (_parent: object, _args: any, context: any) =>
+      context.getUser(),
+
+    /* getUsers: async (
       _parent: object,
-      args:any,
-      { models }: { models: iModels}
+      _args: object,
+      { models }: { models: iModels }
     ): Promise<any> => {
-      const users = await db.User.findAll()  //vaya a saber por qué no anda con models o User, así funcó
+      const users = await models.User.findfindAll();
       return users;
-    }
-    
+    }, */
   },
   Mutation: {
-    createUser: (
+    /* createUser: (
       _: any,
       { input }: { input: any },
       { models }: { models: any }
-    ): any => models.User.create({ ...input }),
+    ): any => models.User.create({ ...input }), */
     deleteUser: async (
       _parent: object,
       { id }: { id: string },
@@ -53,13 +53,13 @@ export default {
 
       return null;
     },
+
     editUser: async (
       _parent: object,
       { id, input }: { id: string; input: iEditUserInput },
       { models }: { models: iModels }
     ): Promise<any> => {
       const UserToEdit = await models.User.findByPk(id);
-
       if (UserToEdit) {
         const updatedUser = await UserToEdit.update(
           { ...input },
@@ -71,58 +71,63 @@ export default {
 
       return null;
     },
-    logout:(parent:object, args:any, context:any) => context.logout(),
 
+    logout: (parent: object, args: any, context: any) => context.logout(),
 
     //llamamos a la funcion authenticate que está en el contexto. le pasamos el nombre de la estrategia que vamos a usar
     //(graphql-local), y las credenciales que podemos leer de las variables de la mutación. Para crear una sesión persistente,
     //passport necesita que llamemos a la funcion login después de autenticar.
-    login: async(
+    login: async (
       _parent: object,
-      { email, password}: { email: string, password: string},
-      context: any ) =>{
-        const { user } = await context.authenticate('graphql-local', { email, password });
-        await context.login(user);
-        return { user }
-      },
-    
+      { email, password }: { email: string; password: string },
+      context: any
+    ) => {
+      const { user } = await context.authenticate("graphql-local", {
+        email,
+        password,
+      });
+      await context.login(user);
+      return { user };
+    },
 
     signup: async (
-    _parent: object,
-    {input }: { input: iSignUpInput},
-    context: any,
+      _parent: object,
+      { firstName, lastName, email, password }: any,
+      context: any
     ): Promise<any> => {
-      const existingUsers = await db.User.findAll();
-      const userWithEmailAlreadyExists = !!existingUsers.find((user:any) => user.dataValues.email === input.email)
+      const existingUsers = await context.models.User.findAll();
+      const userWithEmailAlreadyExists = !!existingUsers.find(
+        (user: any) => user.email === email
+      );
 
-      if(userWithEmailAlreadyExists){
-        throw new Error('User with email already exists');
+      if (userWithEmailAlreadyExists) {
+        throw new Error("User with email already exists");
       }
 
-      console.log(input)
+      // console.log(input);
 
       let newUserInput: any = {
         id: uuid(),
-        name: input.name,
-        surname: input.surname,
-        email: input.email,
-        privilege: 'user',
+        name: firstName,
+        surname: lastName,
+        email: email,
+        privilege: "user",
         active: true,
-        password: input.password,
-        address: input.address,
-        username: input.username
-      }
-      
-      console.log(newUserInput);
-      let newUser = context.models.User.create({ 
-        newUserInput 
-       })
+        password: password,
+        address: "input.address",
+        username: "input.username",
+      };
+
+      // console.log(newUserInput);
+      let newUser = context.models.User.create({
+        newUserInput,
+      });
 
       //console.log(context)
 
       await context.login(newUser);
 
-      return {user: newUser}
+      return { user: newUser };
     },
   },
 };
