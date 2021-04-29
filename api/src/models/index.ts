52c728130ca898380b0b81e6321986cf94e9902b
CrisSoria <crissoria07@gmail.com>
2021-04-29 10:58:14 -0300

    commit inicial

--- api/src/models/index.ts
+++ api/src/models/index.ts
@@ -1,33 +1,36 @@
-import {Sequelize} from 'sequelize'
+import { Sequelize } from "sequelize";
 
-import {CategoryFactory, Category as CategoryClass} from './Category'
-import {ProductFactory, Product as ProductClass} from './Product'
+import { CategoryFactory, Category as CategoryClass } from "./Category";
+import { ProductFactory, Product as ProductClass } from "./Product";
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 export interface DB {
-    sequelize : Sequelize,
-    Product: typeof ProductClass,
-    Category: typeof CategoryClass
-} 
+  sequelize: Sequelize;
+  Product: typeof ProductClass;
+  Category: typeof CategoryClass;
+}
 
-const {DB_NAME, DB_PORT, DB_PASSWORD, DB_URL, DB_USER} = process.env
+const { DB_NAME, DB_PORT, DB_PASSWORD, DB_URL, DB_USER } = process.env;
 
 export const sequelize = new Sequelize(
-    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_URL}:${DB_PORT}/${DB_NAME}`
-)
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_URL}:${DB_PORT}/${DB_NAME}`
+);
 
 const Product = ProductFactory(sequelize);
 const Category = CategoryFactory(sequelize);
 
-Product.belongsToMany(Category,{through: 'productsxcategories'});
-Category.belongsToMany(Product,{through: 'productsxcategories'})
+Product.belongsToMany(Category, { through: "productsxcategories" });
+Category.belongsToMany(Product, { through: "productsxcategories" });
 
-const db : DB = {
-    sequelize,
-    Product,
-    Category,
+//arbol de compatibilidades
+Product.belongsToMany(Product, { through: "productTree" });
+
+const db: DB = {
+  sequelize,
+  Product,
+  Category,
 };
 
 export default db;
