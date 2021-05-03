d02c4226f7b9aeca52d556523faa899996501e2a
AndresLLogares <andresl940@hotmail.com>
2021-05-03 18:50:14 -0300

    Lunes 3/5 18:50

--- client/src/components/Users/AdminDelete.tsx
+++ client/src/components/Users/AdminDelete.tsx
@@ -125,7 +125,7 @@ const AdminDelete = () => {
                                     <div className={styles3.sortUser} >
                                         <p className={styles3.UserP} ><FontAwesomeIcon icon={faFileSignature} /> Nombre: {item.name}</p>
                                         <p className={styles3.UserP} ><FontAwesomeIcon icon={faEnvelopeSquare} /> E-Mail: {item.email}</p>
-                                        <p className={styles3.UserP} ><FontAwesomeIcon icon={faMapMarker} />Direccion: {item.address}</p>
+                                        <p className={styles3.UserP} ><FontAwesomeIcon icon={faMapMarker} />Direccion: {item.street}</p>
                                         <p className={styles3.UserP} ><FontAwesomeIcon icon={faShareAlt} />Nombre de Usuario: {item.username} </p>
                                         <p className={styles3.UserP} ><FontAwesomeIcon icon={faCrown} /> Nivel: {item.privilege}</p>
                                     </div>
